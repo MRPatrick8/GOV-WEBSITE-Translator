91eac75e04ae475b24ae882825bb5133180be4f9
MRPatrick8 <renepatrick978@gmail.com>
2023-03-31 13:21:27 +0200

    updated the interface

--- main.py
+++ main.py
@@ -4,19 +4,40 @@ import pandas as pd
 from accuracy import *
 
 
-st.title("GOV Website Translation")
+st.title("GOV Website Contents Translation")
 
 
-with st.sidebar:
-    st.header("Choose an option")
+# with st.sidebar:
+#     st.header("Choose an option")
+# Add CSS styling to center the element
+st.markdown(
+    """
+    <style>
+    .center {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        height: 80px;
+        margin: 0 auto;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
 
+# Add the file uploader element inside a div with the center class
+with st.container():
+    st.markdown('<div class="center">', unsafe_allow_html=True)
+    file_upload = st.file_uploader('Upload CSV file', type=['csv'])
+    st.markdown('</div>', unsafe_allow_html=True)
 
-file_upload = st.file_uploader('Upload CSV file', type=['csv', 'html'])
+# file_upload = st.file_uploader('Upload CSV file', type=['csv', 'html'])
 
 
 def process_upload(df):
-    df['translated_text']=translate_dataframe(df, target_lang='rw', source_lang='en')
-    df['Back_translated_text']=back_translate_dataframe(df, target_lang='en', source_lang='rw')
+    with st.spinner("Translating ......."):
+        df['translated_text']=translate_dataframe(df, target_lang='rw', source_lang='en')
+        df['Back_translated_text']=back_translate_dataframe(df, target_lang='en', source_lang='rw')
 
     return df
 
@@ -45,14 +66,14 @@ if file_upload:
 
         #Save the output
         df.to_csv('output.csv')
-        @st.cache
+        @st.cache_data
         def convert_df(df):
             # IMPORTANT: Cache the conversion to prevent computation on every rerun
             return df.to_csv().encode('utf-8')
 
         csv = convert_df(df)
 
-        st.download_button(label="Download data as CSV",file_name='output.csv',data=csv, mime='text/csv')
+        st.download_button(label="Download a CSV",file_name='output.csv',data=csv, mime='text/csv')
 
 
 
