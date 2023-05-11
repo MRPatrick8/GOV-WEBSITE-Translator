830d69117e9cad53489e2cabf4f8e8885547edf7
MRPatrick8 <renepatrick978@gmail.com>
2023-05-12 01:38:45 +0200

    completed

--- main.py
+++ main.py
@@ -1,90 +1,10 @@
-# import streamlit as st 
-# from translator import *
-# import pandas as pd
-# from accuracy import *
-# from docx import Document
-# # from annotated_text import annotated_text
-
-
-# st.title("GOV Contents Translation")
-
-# col1, col2 = st.columns(2)
-
-# with col1:
-#    st.image("https://www.developmentaid.org/files/organizationLogos/huzalabs-435178.jpg")
-
-# with col2:
-#    st.image("images/Rwanda Gov.png")
-
-# st.image("images/GIZ_HUZALABS_FAIRFORWARD.png")
-
-# # with st.sidebar:
-# #     st.header("Choose an option")
-# # Add CSS styling to center the element
-# st.markdown(
-#     """
-#     <style>
-#     .center {
-#         display: flex;
-#         justify-content: center;
-#         align-items: center;
-#         height: 80px;
-#         margin: 0 auto;
-#     }
-#     </style>
-#     """,
-#     unsafe_allow_html=True,
-# )
-
-# # Add the file uploader element inside a div with the center class
-# with st.container():
-#     st.header("Upload your Docx file")
-#     file_upload = st.file_uploader('', type=['docx'])
-    
-
-#     #save file file
-#     if file_upload:
-#         with open('data/content.docx', "wb+") as doc_file:
-#             doc_file.write(file_upload.read())
-#         #Read the doc file
-#         doc = Document('data/content.docx')
-#         paragraphs = []
-#         for i in range(len(doc.paragraphs)):
-#             paragraphs.append(doc.paragraphs[i].text)
-#         st.session_state['text_area'] = "\n".join(paragraphs)
-
-#     st.header("Or Paste/Type your Text Here")
-#     #Paste in your text docs
-#     st.text_area("", key='text_area', height=10)
-
-
-    
-    
-# translate_button, download_button, a ,b  = st.columns(4)
-
-# with translate_button:
-#     translate_button = st.button("Translate Dataset")
-   
-       
-# if translate_button:
-#     paragraphs = st.session_state['text_area'].split('\n')
-#     texts = []
-#     for text in paragraphs:
-#         trans_text = translate_row(text, target_lang='rw', source_lang='en')
-#         texts.append(trans_text)
-
-#     #print(texts)
-#     st.text_area("Translation", value='\n'.join(texts), disabled=False, height=20)
-
-
-# with download_button:
-#     download_button = st.button("Download Dataset")
 import streamlit as st 
 from translator import *
 import pandas as pd
 from accuracy import *
 from docx import Document
-# from annotated_text import annotated_text
+from io import BytesIO
+import re
 
 
 st.title("GOV Contents Translation")
@@ -119,42 +39,59 @@ st.markdown(
 
 # Add the file uploader element inside a div with the center class
 with st.container():
-    st.header("Upload your Docx file")
-    file_upload = st.file_uploader('', type=['docx'])
+    st.header("Upload your file")
+    file_upload = st.file_uploader('', type=['docx', 'txt'])
     
 
     #save file file
     if file_upload:
-        with open('data/content.docx', "wb+") as doc_file:
-            doc_file.write(file_upload.read())
-        #Read the doc file
-        doc = Document('data/content.docx')
-        paragraphs = []
-        for i in range(len(doc.paragraphs)):
-            paragraphs.append(doc.paragraphs[i].text)
-        st.session_state['text_area'] = "\n".join(paragraphs)
+        if file_upload.type == 'text/plain': # For txt files
+            text = file_upload.read().decode('utf-8')
+            st.session_state['text_area'] = text
+        else: # For docx files
+            with open('data/content.docx', "wb+") as doc_file:
+                doc_file.write(file_upload.read())
+            #Read the doc file
+            doc = Document('data/content.docx')
+            paragraphs = []
+            for i in range(len(doc.paragraphs)):
+                paragraphs.append(doc.paragraphs[i].text)
+            st.session_state['text_area'] = "\n".join(paragraphs)
 
     st.header("Or Paste/Type your Text Here")
     #Paste in your text docs
     st.text_area("", key='text_area', height=10)
-
-
-    
-    
-from io import BytesIO
+   
 
 translate_button, download_button, a, b = st.columns(4)
 
 with translate_button:
-    translate_button = st.button("Translate Dataset")
+    translate_button = st.button("Translate")
+
+def clean_text(text):
+    # Remove non-alphabetic characters
+    text = re.sub('[^a-zA-Z\s]', '', text)
+    # Convert to lowercase
+    text = text.lower()
+    # Remove extra spaces
+    text = re.sub('\s+', ' ', text)
+    # Remove leading/trailing spaces
+    text = text.strip()
+    return text
 
 if translate_button:
     if 'text_area' in st.session_state:
         paragraphs = st.session_state['text_area'].split('\n')
         texts = []
-        for text in paragraphs:
-            trans_text = translate_row(text, target_lang='rw', source_lang='en')
-            texts.append(trans_text)
+        progress_bar = st.progress(0)
+        for i, text in enumerate(paragraphs):
+            if text:
+                # Clean the text before translating
+                cleaned_text = clean_text(text)
+                trans_text, accuracy = translate_row(cleaned_text, target_lang='rw', source_lang='en')
+                texts.append(trans_text)
+            # Update the progress bar
+            progress_bar.progress(int((i+1)/len(paragraphs)*100))
         if len(texts) > 0:
             st.text_area("Translation", value='\n'.join(texts), disabled=False, height=20)
             st.session_state['translated_texts'] = texts
@@ -163,12 +100,16 @@ if translate_button:
 
 with download_button:
     download_button = st.button("Download Dataset")
-
+if 'accuracies' not in st.session_state:
+    st.session_state['accuracies'] = []
 if download_button:
     if 'translated_texts' in st.session_state:
         doc = Document()
-        for para in st.session_state['translated_texts']:
+        for i, para in enumerate(st.session_state['translated_texts']):
             doc.add_paragraph(para)
+            if 'accuracies' in st.session_state and i < len(st.session_state['accuracies']):
+                if st.session_state['accuracies'][i] == min(st.session_state['accuracies']):
+                    doc.paragraphs[i].style = 'Warning Text' #highlight the lowest accuracy paragraph
         doc_io = BytesIO()
         doc.save(doc_io)
         doc_io.seek(0)
--- translator.py
+++ translator.py
@@ -1,3 +1,35 @@
+# import requests
+# import json
+# import string
+# import pandas as pd
+# import docx
+# import re
+
+# class GoogleTranslate:
+#     api_url = 'https://translate.googleapis.com/translate_a/single'
+#     client = '?client=gtx'
+#     dt = '&dt=t'
+
+# def translate_row(text, target_lang, source_lang):
+#     sl = f"&sl={source_lang}"
+#     tl = f"&tl={target_lang}"
+    
+#     r = requests.get(f"{GoogleTranslate.api_url}{GoogleTranslate.client}{GoogleTranslate.dt}{sl}{tl}&q={text}")
+#     if r.status_code == 200:
+#         response_data = json.loads(r.text)
+#         translated_text = response_data[0][0][0]
+#     else:
+#         translated_text = ""
+#     return translated_text
+
+# def translate_paragraphs(input_paragraphs, target_lang, source_lang=None):
+#     paragraphs = []
+#     for paragraph in input_paragraphs:
+#         trans_paragraph = translate_row(paragraph, target_lang, source_lang)
+#         paragraphs.append(trans_paragraph)
+
+#     return paragraphs
+
 import requests
 import json
 import string
@@ -14,80 +46,33 @@ def translate_row(text, target_lang, source_lang):
     sl = f"&sl={source_lang}"
     tl = f"&tl={target_lang}"
     
-    # Remove punctuation marks from the input text
-    # translator = str.maketrans('', '', string.punctuation)
-    # text = row['content'].translate(translator)
-    
     r = requests.get(f"{GoogleTranslate.api_url}{GoogleTranslate.client}{GoogleTranslate.dt}{sl}{tl}&q={text}")
     if r.status_code == 200:
         response_data = json.loads(r.text)
         translated_text = response_data[0][0][0]
+        accuracy = response_data[0][0][2]
     else:
         translated_text = ""
-    return translated_text
-
-def translate_document(input_file, output_file, target_lang, source_lang):
-    # Create a new Word document
-    document = docx.Document()
-    
-    # Read the input document and create a list of paragraphs
-    paragraphs = []
-    input_doc = docx.Document(input_file)
-    for para in input_doc.paragraphs:
-        paragraphs.append(para.text)
-    
-    # Create a dataframe from the paragraphs
-    df = pd.DataFrame({'content': paragraphs})
-    
-    # Translate the dataframe and add the translated text to the dataframe
-    df['translated_text'] = translate_dataframe(df, target_lang, source_lang)
-    
-    # Add the translated text to the output document
-    for index, row in df.iterrows():
-        document.add_paragraph(row['translated_text'])
-    
-    # Save the output document
-    document.save(output_file)
+        accuracy = 0.0
+    return translated_text, accuracy
 
 def translate_paragraphs(input_paragraphs, target_lang, source_lang=None):
     paragraphs = []
+    accuracies = []
     for paragraph in input_paragraphs:
-        trans_paragraph = translate_row(paragraph, target_lang, source_lang)
-        paragraphs.append(paragraphs)
+        trans_paragraph, accuracy = translate_row(paragraph, target_lang, source_lang)
+        paragraphs.append(trans_paragraph)
+        accuracies.append(accuracy)
 
+    # Find the lowest accuracy and highlight the corresponding paragraph
+    min_accuracy = min(accuracies)
+    for i, acc in enumerate(accuracies):
+        if acc == min_accuracy:
+            paragraphs[i] = f"*** {paragraphs[i]} ***"  # highlight the paragraph
+
+    # Print the accuracies for each paragraph
+    for i, (paragraph, accuracy) in enumerate(zip(paragraphs, accuracies)):
+        print(f"Paragraph {i+1}: Accuracy = {accuracy:.2f}")
+    
     return paragraphs
 
-def translate_dataframe(df, target_lang, source_lang):
-    df_translated = df.apply(translate_row, axis=1, args=(target_lang, source_lang))
-    return df_translated['translated_text']
-
-############################Back##############################################
-def back_translate_document(input_file, output_file, target_lang, source_lang):
-    # Create a new Word document
-    document = docx.Document()
-    list_of_contents_modified = [re.sub(r'\s+', ' ', re.sub(r'\W+', ' ', item)).strip() for item in input_file]
-    # Read the input document and create a list of paragraphs
-    paragraphs = []
-    input_doc = docx.Document(input_file)
-    for para in input_doc.paragraphs:
-        paragraphs.append(para.text)
-    
-    # Create a dataframe from the paragraphs
-    df = pd.DataFrame({'content': paragraphs})
-    
-    # Translate the dataframe and add the translated text to the dataframe
-    df['back_translated_text'] = translate_dataframe(df, target_lang, source_lang)
-    
-    # Add the translated text to the output document
-    for index, row in df.iterrows():
-        document.add_paragraph(row['back_translated_text'])
-    
-    # Save the output document
-    document.save(output_file)
-
-def back_translate_dataframe(df, target_lang, source_lang):
-    df_translated = df.apply(translate_row, axis=1, args=(target_lang, source_lang))
-    return df_translated['back_translated_text']
-# Translate the input document and save the output document
-# translate_document('content_cleaned.docx', 'output_file.docx', 'rw', 'en')
-# back_translate_document('output_file.docx', 'back_output_file.docx', 'en', 'rw')
